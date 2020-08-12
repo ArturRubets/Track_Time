575f6405f2ff6b9620839450648e39359d25fb47
Artur Rubec <Artur.Rubec@livenau.net>
2020-08-12 21:23:32 +0300

    creating data context

---
+++ Track_Time/EF/ContextData.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Track_Time.Models;
+
+namespace Track_Time.EF
+{
+    public class ContextData : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}

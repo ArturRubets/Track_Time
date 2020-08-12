cbe97dff1b474ef536111a1ac89926044627cf6c
Artur Rubec <Artur.Rubec@livenau.net>
2020-08-12 21:01:38 +0300

    added entities

---
+++ Track_Time/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Track_Time.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        [Required]
+        [RegularExpression(@"[MW]", ErrorMessage = "Invalid. M or W")]
+        public char Sex { get; set; }
+    }
+}
---
+++ Track_Time/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Track_Time.Models
+{
+    public class Project
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime DateStart { get; set; }
+        [Required]
+        public DateTime DateEnd { get; set; }
+    }
+}
